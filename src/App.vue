36e7e7f4841478cce68f88dd4215d2d874c00976
Carlos Augusto <carlos.silva@hypnobox.com.br>
2020-04-27 09:17:46 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app" id="app">
+    <Header />
+    <Menu />
+
+    <div class="main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Menu from './components/Menu'
+// import Banner from './components/Banner'
+
+export default {
+  components: { Header, Menu },
+  created: function () {
+    window.addEventListener('resize',this.windowResize);
+  },
+  destroyed: function () {
+    window.removeEventListener('resize', this.windowResize);
+  },
+  methods: {
+    windowResize () {
+      let sizes = {
+        h: window.innerHeight,
+        w: window.innerWidth,
+      }
+
+      this.$store.dispatch('resizeWindow', sizes).then(() => false);
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './assets/css/app.scss';
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda blanditiis dolore ea illo in, libero quia? Enim excepturi, fugit id incidunt modi odio quas quia quibusdam, repellat similique sit unde.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>

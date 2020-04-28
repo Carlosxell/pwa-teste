7af76890b6da4200bb22ca029734a04735492c0c
Carlos Augusto <carlos.silva@hypnobox.com.br>
2020-04-28 16:02:37 -0300

    atulização de serviços do PWA

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,5 @@ module.exports = {
     themeColor: '#2d3549',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-  },
-  chainWebpack(config) {
-    config.plugin('html').tap(args => {
-      args[0].title = '99 Leads'
-      return args;
-    });
   }
 };
